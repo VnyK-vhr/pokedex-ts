cd56c0afba53dd0ea6d04bee77939a72d394a76b
Vinayak <vn26vhr@gmail.com>
2023-01-31 21:38:45 +0530

    Refactor Pagination.tsx

--- src/components/Paginator.tsx
+++ src/components/Paginator.tsx
@@ -1,12 +1,29 @@
-import { Box, Pagination } from "@mui/material";
+import { Box as MuiBox, styled, Pagination } from "@mui/material";
 import usePokedex from "../pokedex";
+
+const Box = styled(MuiBox)(({ theme }) => ({
+  margin: "auto",
+  padding: theme.spacing(2, 0),
+  [theme.breakpoints.down("md")]: {
+    margin: theme.spacing(2, "auto"),
+  },
+}));
 
 export default function Paginator() {
   const pokemon = usePokedex((state) => state.currentPokemon);
   const setPokemon = usePokedex((state) => state.setPokemon);
 
   return (
-    <Box sx={{ mx: "auto", py: 2, mt: "auto" }}>
+    <Box
+      sx={{
+        mx: "auto",
+        py: 2,
+        mt: "auto",
+        "@media only screen and (max-width: 200px)": {
+          mt: 2,
+        },
+      }}
+    >
       <Pagination
         count={1008}
         boundaryCount={2}
